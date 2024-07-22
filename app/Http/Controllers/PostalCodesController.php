f6b11d5d50ff1058848d57d9dee918a211372f86
Eduardo Vasconcelos <c.eduvasconcelos@gmail.com>
2024-07-22 02:21:00 -0300

    Display results from postal code API

--- app/Http/Controllers/PostalCodesController.php
+++ app/Http/Controllers/PostalCodesController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\PostalCodeApi;
 use Illuminate\Http\JsonResponse;
 
 class PostalCodesController extends Controller
 {
+    protected PostalCodeApi $postalCodeApi;
+
+    public function __construct(PostalCodeApi $postalCodeApi)
+    {
+        $this->postalCodeApi = $postalCodeApi;
+    }
 
     public function search(string $postalCodes): JsonResponse
     {
-        return response()->json();
+        $postalCodesFormatted = explode(',', $postalCodes);
+        $response = [];
+
+        foreach ($postalCodesFormatted as $postalCode) {
+            $response[] = $this->postalCodeApi->search($postalCode);
+        }
+
+        return response()->json($response);
     }
 }
